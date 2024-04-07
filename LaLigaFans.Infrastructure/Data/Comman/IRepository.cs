e4894af01e69326865c3c57afa70b90fad52bb34
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2024-04-07 18:53:55 +0300

    Added unit of work repository with generic methods for working with all entity classes

---
+++ LaLigaFans.Infrastructure/Data/Comman/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaLigaFans.Infrastructure.Data.Comman
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllReadOnly<T>() where T : class;
+
+        Task AddAsync<T>(T entity) where T : class;
+
+        Task DeleteAsync<T>(object id) where T : class;
+
+        Task<T?> GetByIdAsync<T>(object id) where T : class;
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ LaLigaFans.Infrastructure/Data/Comman/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaLigaFans.Infrastructure.Data.Comman
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+
+        public Repository(DbContext _context)
+        {
+            context = _context;
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return GetDbSet<T>();
+        }
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return GetDbSet<T>()
+                    .AsNoTracking();
+        }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await GetDbSet<T>().AddAsync(entity);
+        }
+
+        public async Task DeleteAsync<T>(object id) where T : class
+        {
+            T? entity = await GetByIdAsync<T>(id);
+            if(entity != null)
+            {
+                GetDbSet<T>().Remove(entity);
+            }
+        }
+
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class
+        {
+            return await GetDbSet<T>().FindAsync(id);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+
+
+        private DbSet<T> GetDbSet<T>() where T : class
+        {
+            return context.Set<T>();
+        }
+    }
+}
