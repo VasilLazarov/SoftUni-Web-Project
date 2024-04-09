b9b4f0fea2811ae799c4aab1d6d1fe728809db4f
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2024-04-09 15:34:16 +0300

    Remove and sort usings in some classes

--- LaLigaFans.Infrastructure/Data/Comman/IRepository.cs
+++ LaLigaFans.Infrastructure/Data/Comman/IRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LaLigaFans.Infrastructure.Data.Comman
+﻿namespace LaLigaFans.Infrastructure.Data.Comman
 {
     public interface IRepository
     {
