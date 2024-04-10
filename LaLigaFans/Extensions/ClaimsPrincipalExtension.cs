6bf5236df490803c4906f525110e2659e4fb3970
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2024-04-10 15:13:23 +0300

    Added claims principal extension with two methods, to get the Id of a user and to check if the current user is an admin

---
+++ LaLigaFans/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static LaLigaFans.Infrastructure.Constants.AdminConstants;
+
+namespace System.Security.Claims
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRoleName);
+        }
+
+    }
+}
